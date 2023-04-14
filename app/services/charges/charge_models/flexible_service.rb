a3a6d770a05f1a135862726133780f0e8a2099ac
Suraj Mittal <mittal.suraj18@gmail.com>
2023-04-14 19:01:58 +0530

    add per amount based slabs inside volume based slabs

--- app/services/charges/charge_models/flexible_service.rb
+++ app/services/charges/charge_models/flexible_service.rb
@@ -7,28 +7,72 @@ module Charges
         properties['graduated_ranges']&.map(&:with_indifferent_access)
       end
 
+      def select_slab(amount, amount_slabs)
+        selected_slab = nil
+        amount_slabs.each { |slab|
+          if (!slab["from_value"].nil? && !slab["to_value"].nil?) && (slab["from_value"] <= amount && amount <= slab["to_value"])
+            selected_slab = slab
+            break
+          end
+          if (!slab["from_value"].nil? && slab["to_value"].nil?) && slab["from_value"] <= amount
+            selected_slab = slab
+            break
+          end
+          if (!slab["to_value"].nil? && slab["from_value"].nil?) and amount <= slab["to_value"]
+            selected_slab = slab
+            break
+          end
+          if slab["to_value"].nil? and slab["from_value"].nil?
+            selected_slab = slab
+            break
+          end
+          selected_slab
+        }
+      end
+
+      def compute_slab_amount(amount, selected_slab)
+        result_amount = 0
+        per_unit_amount = BigDecimal(selected_slab[:per_unit_amount] || 0)
+        result_amount += per_unit_amount
+
+        percentage_rate = BigDecimal(selected_slab[:percentage_rate] || 0)
+        percentage_amount = amount * percentage_rate / 100
+
+        per_txn_fixed_fee_deductions = selected_slab[:per_txn_fixed_fee_deductions] || 0
+        min_amount = selected_slab[:min_amount]
+        max_amount = selected_slab[:max_amount]
+
+        if min_amount and max_amount
+          percentage_amount = min(max_amount, max(percentage_amount, min_amount)) - per_txn_fixed_fee_deductions
+        elsif min_amount
+          percentage_amount = max(percentage_amount, min_amount) - per_txn_fixed_fee_deductions
+        elsif max_amount
+          percentage_amount = min(max_amount, percentage_amount) - per_txn_fixed_fee_deductions
+        else
+          percentage_amount = percentage_amount - per_txn_fixed_fee_deductions
+        end
+        result_amount + percentage_amount
+      end
+
       def compute_per_txn_aggregation
         ranges.reduce(0) do |result_amount, range|
-          flat_amount = BigDecimal(range[:flat_amount])
-          per_unit_amount = BigDecimal(range[:per_unit_amount])
-          percentage_rate = BigDecimal(range[:percentage_rate] || 0)
-          min_amount = range[:min_amount]
-          max_amount = range[:max_amount]
-          fixed_fee_deductions = BigDecimal(range[:per_txn_fixed_fee_deductions] || 0)
+          flat_amount = BigDecimal(range[:flat_amount] || 0)
 
+          amount_slabs = range[:amount_slabs]
+          from_value = range[:from_value]
+          to_value = range[:to_value]
+          amounts_between_volume_range = aggregation_result.events[from_value..to_value]
+
+          result_amount = 0
           # NOTE: Add flat amount to the total
           result_amount += flat_amount unless units.zero?
-
-          range_units = compute_range_units(range[:from_value], range[:to_value])
-          result_amount += range_units * per_unit_amount
-
-          percentage_units = compute_percentage_units(percentage_rate, range[:from_value], range[:to_value], min_amount, max_amount, fixed_fee_deductions)
-          result_amount += percentage_units
-
+          amounts_between_volume_range.each { |amount|
+            selected_slab = select_slab(amount, amount_slabs)
+            result_amount += compute_slab_amount(amount, selected_slab)
+          }
           # NOTE: aggregation_result.aggregation is between the bounds of the current range,
           #       we must stop the loop
           break result_amount if range[:to_value].nil? || range[:to_value] >= units
-
           result_amount
         end
       end
@@ -50,34 +94,6 @@ module Charges
         a < b ? a : b
       end
 
-      def compute_percentage_units(percentage_rate, from_value, to_value, min_amount, max_amount, fixed_fee_deductions)
-        total = 0.0
-        percent = percentage_rate / 100
-        if min_amount and max_amount
-          aggregation_result.events[from_value..to_value].map { |x| total += min(max_amount, max(x, min_amount)) * percent - fixed_fee_deductions }
-        elsif min_amount
-          aggregation_result.events[from_value..to_value].map { |x| total += max(x, min_amount) * percent - fixed_fee_deductions }
-        elsif max_amount
-          aggregation_result.events[from_value..to_value].map { |x| total += min(max_amount, x) * percent - fixed_fee_deductions }
-        else
-          aggregation_result.events[from_value..to_value].map { |x| total += x * percent - fixed_fee_deductions }
-        end
-        total
-      end
-
-      # NOTE: compute how many units to bill in the range
-      def compute_range_units(from_value, to_value)
-        # NOTE: units is higher than the to_value of the range
-        if to_value && units >= to_value
-          return to_value - (from_value.zero? ? 1 : from_value) + 1
-        end
-
-        return to_value - from_value if to_value && units >= to_value
-        return units if from_value.zero?
-
-        # NOTE: units is in the range
-        units - from_value + 1
-      end
     end
   end
 end
